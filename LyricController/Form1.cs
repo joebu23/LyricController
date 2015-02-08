eda5aa9d75ae7f78172ff71ef93e54b6b3af4dc1
joebu23 <joe.royston@gmail.com>
2015-02-07 20:19:51 -0500

    Adding and testing telnet connectivity

--- LyricController/Form1.cs
+++ LyricController/Form1.cs
@@ -13,11 +13,31 @@ namespace LyricController
 {
     public partial class Form1 : Form
     {
+        TelnetConnection Lyric = new TelnetConnection("127.0.0.1", 23);
+
         public Form1()
         {
             InitializeComponent();
-            TelnetConnection Lyric = new TelnetConnection("127.0.0.1", 23);
+            
+        }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Lyric.WriteLine("testing");
+            //MessageBox.Show("hi");
+            //Console.Write("hi");
+            //while (tc.IsConnected && prompt.Trim() != "exit")
+            //{
+            //    // display server output
+            Console.Write(Lyric.Read());
+
+            //    // send client input to server
+            //    prompt = Console.ReadLine();
+            //    tc.WriteLine(prompt);
+
+            //    // display server output
+            //    Console.Write(tc.Read());
+            //}
         }
     }
 }
